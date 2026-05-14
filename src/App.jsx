5070f6a4183b545f4c5fd7323999014f06479dd4
Denaz <denibrea7@gmail.com>
2026-05-14 16:59:46 +0200

    added multiple cards for the next 7 days

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import useWeather from './hooks/useWeather'
 import {useState, useEffect} from 'react'
 
 
-
 function App() {
 
+  const [selectedDay, setSelectedDay] = useState(0);
   const [inputValue, setInputValue] = useState('');
   const [city, setCity] = useState('');
   const coordinates = useCoordinates(city);
@@ -31,7 +31,15 @@ function App() {
   return (
     <>
     <SearchBar onChange={(e) => setInputValue(e.target.value)}/>
-    <WeatherCard weather={weather} city={coordinates?.name}/>
+    <div className="flex flex-row">
+      <WeatherCard weather={weather} city={coordinates?.name} index={0} selectedDay={selectedDay} onClick={() => setSelectedDay(0)} onMouseEnter/>
+      <WeatherCard weather={weather} city={coordinates?.name} index={1} selectedDay={selectedDay} onClick={() => setSelectedDay(1)}/>
+      <WeatherCard weather={weather} city={coordinates?.name} index={2} selectedDay={selectedDay} onClick={() => setSelectedDay(2)}/>
+      <WeatherCard weather={weather} city={coordinates?.name} index={3} selectedDay={selectedDay} onClick={() => setSelectedDay(3)}/>
+      <WeatherCard weather={weather} city={coordinates?.name} index={4} selectedDay={selectedDay} onClick={() => setSelectedDay(4)}/>
+      <WeatherCard weather={weather} city={coordinates?.name} index={5} selectedDay={selectedDay} onClick={() => setSelectedDay(5)}/>
+      <WeatherCard weather={weather} city={coordinates?.name} index={6} selectedDay={selectedDay} onClick={() => setSelectedDay(6)}/>
+    </div>
     </>
   ) 
 
--- src/components/Weather/WeatherCard/index.jsx
+++ src/components/Weather/WeatherCard/index.jsx
@@ -1,6 +1,6 @@
 import IconWeather from "./IconWeather";
 
-function WeatherCard({weather, city}) {
+function WeatherCard({weather, city, index, selectedDay, onClick}) {
 
     function rightHour() {
         const date = new Date();
@@ -9,9 +9,17 @@ function WeatherCard({weather, city}) {
         return hour;
     }
 
-    return <div className="flex flex-col items-center p-4 rounded-md w-60 sm:px-12 bg-sky-900 m-4">
+    function rightDay() {
+        const date = new Date();
+        date.setDate (date.getDate() + index);
+
+        return date;
+    }
+
+    if (index == selectedDay) {
+    return <div onClick={onClick} className="flex flex-col items-center p-4 rounded-md w-40 sm:px-12 bg-sky-900 m-2">
         <div className="text-center">
-            <h2 className="text-left text-sm text-sky-200">{new Date().toLocaleDateString()}</h2>
+            <h2 className="text-left text-sm text-sky-200">{rightDay().toLocaleDateString("en-GB")}</h2>
         </div>    
         <div className="text-center">
             <h2 className="text-xl text-sky-200">{city}</h2>
@@ -22,6 +30,16 @@ function WeatherCard({weather, city}) {
         <div className="mb-2 text-3xl font-semibold text-cyan-400">{weather?.hourly?.temperature_2m[rightHour()]}°C
         </div>
     </div>  
+    }
+
+    else {
+        return <div onClick={onClick} className="flex flex-col items-center p-4 rounded-md w-25 sm:px-12 bg-sky-900 m-2">
+        <div className="text-center">
+            <h2 className="text-left text-sm font-size- text-sky-200">{rightDay().toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit" })}</h2>
+        </div>
+    </div>  
+    }
+        
 }
 
 export default WeatherCard;
